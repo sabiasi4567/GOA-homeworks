d4177b6cbc53b16d7bb6424b98225eefa6116572
sabias4567 <i.tsitskishvili1987@gmail.com>
2024-10-04 10:36:09 -0700

    commit

---
+++ day1/lesson 1.py
@@ -0,0 +1,34 @@
+name = "saba"
+#name არის ცვლადი
+# = არის ცვლადისტვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "saba" არის ცვლადისთვის მნიშვნელობა
+
+
+surname = "lomsadze"
+
+#print(name)
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ცვლადი ან სტრინგი
+
+
+name = "Saba" #ეს არის str (string) ტიპის ცვლადი
+age = 13 #ეს არის int (integer) მთელი რიცხვი
+height = 156.0 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #True ან False
+is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False #ჯავასკრიპტული camelcase
+
+print(name + " " + surname)
+
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print("მე ვარ " + name + ", ვარ " + str(age) + " წლის " + ", ჩემი გვარია " + str(surname) + ", ჩემი სიმაღლეა " + str(height) )
+
