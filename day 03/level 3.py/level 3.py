08ef0a21ce0411810bc455b279cd1cb5f901ed0e
sabias4567 <i.tsitskishvili1987@gmail.com>
2024-10-23 08:06:47 -0700

    commit

---
+++ day 03/level 3.py/level 3.py
@@ -0,0 +1,15 @@
+name=("saba")
+lastname=("lomsadze")
+age=(13)
+favouriteanimal=("dog")
+
+
+
+print(name)
+print(lastname)
+print(age)
+print(favouriteanimal)
+
+
+
+print(name , lastname , age , favouriteanimal)
---
+++ day 03/level 3/level 3.py
@@ -0,0 +1,12 @@
+name="me var saba "
+
+lastname=" lomsadze var "
+
+age= " 13  wlis"
+
+hobby=" chemi  hobia fegburtis tamashi"
+
+country="me vcxovrob sacartveloshi"
+
+
+print(name + lastname + age + hobby + country)
