9b4c70818124cca62ff205d255760c6318fa43ab
sabias4567 <i.tsitskishvili1987@gmail.com>
2024-11-21 05:11:30 -0800

    commit

---
+++ day 07/level 7.py
@@ -0,0 +1,32 @@
+#input(შეტანა)-output(გამოტანა)
+
+#input-ფუმქცია რომლის მეშვეობუით მომხმარებელს შემოაქ რაღც
+
+#name=input("შეიყვანე შენი სახელი:") #input
+#print("გამარჯობა", name) #output
+
+
+#-----------------------------------------------
+
+
+
+#name=input("შეიყვანე შენი სახელი:")
+#password=input("შეიყვანე შენი პაროლი:")
+
+
+
+
+
+
+
+
+
+print("C A L C U L A T O R")
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
