95791254bbdc987fad36220e0a0fb67ed14853f3
sabias4567 <i.tsitskishvili1987@gmail.com>
2024-12-23 06:35:53 -0800

    commit

---
+++ day 11/classwork.py
@@ -0,0 +1,66 @@
+print(10 > 9) 
+print(10 > 8)
+print(10 >11)
+print(10 >25)
+print(10 >3)
+print(10 >1)
+print(10 >77)
+print(10 >4)
+print(10 >21)
+print(10 >87)
+
+
+
+print(41 < 19)
+print(86 < 24)
+print(95 < 197)
+print(95 < 124)
+print(74 < 32)
+print(63 < 75)
+print(53 < 67)
+print(62 < 15)
+print(65 < 17)
+print(19 < 20)
+
+
+
+
+
+print(856 != 86)
+print(96 != 56)
+print(5 != 24)
+print(2 != 46)
+print(53 != 68)
+print(543 != 356)
+print(67 != 53)
+print(89 != 98)
+print(12 != 75)
+print(54 != 23)
+
+
+
+print(687 == 712)
+print(754 == 754)
+print(54 == 54)
+print(674 == 674)
+print(567 == 567)
+print(753 == 712)
+print(57 ==86)
+print(86 == 12)
+print(94 == 79)
+print(75 ==75)
+
+
+y =("cold") 
+x = ("cold")
+
+print(y == x)
+print(y < x)
+print(y > x)
+print(y != x)
+
+
+
+
+
+
---
+++ day 11/homework.py
@@ -0,0 +1,99 @@
+print(10 > 9) 
+print(10 > 8)
+print(10 > 11)
+print(10 > 25)
+print(10 > 3)
+print(10 > 1)
+print(10 > 77)                        
+print(10 > 4)
+print(10 > 21)
+print(10 > 87)
+print(10 >61 )
+print(10 >6 )
+print(10 >86 ) 
+print(10 >90 ) 
+print(10 >12 )
+
+
+print(41 < 19)
+print(86 < 24)
+print(95 < 197)
+print(95 < 124)
+print(74 < 32)
+print(63 < 75)
+print(53 < 67)
+print(62 < 15)
+print(65 < 17)
+print(19 < 20)
+print(63 < 64)
+print(12 < 321)
+print(35 < 12)
+print(56 <  51)
+print(132 < 625)
+
+
+
+print(856 != 86)
+print(96 != 56)
+print(5 != 24)
+print(2 != 46)
+print(53 != 68)
+print(543 != 356)
+print(67 != 53)
+print(89 != 98)
+print(12 != 75)
+print(54 != 23)
+print(212 != 11)
+print(234 != 41)
+print(121 != 46)
+print(31 != 123)
+print(51 !=51)
+
+
+print(687 == 712)
+print(754 == 754)
+print(54 == 54)
+print(674 == 674)
+print(567 == 567)
+print(753 == 712)
+print(57 ==86)
+print(86 == 12)
+print(94 == 79)
+print(75 ==75)
+print(642 == 98)
+print(151 == 342)
+print(54 == 12)
+print(674 == 471)
+print(74 == 78)
+
+
+#მეორე დავალება
+
+x = ("hello")  
+y = ("helloworld")
+t = ("helloguys")
+U = ("hellofamily")
+i = ("hellofriends")
+p = ("helloladys")
+l = ("hellonerds")
+q = ("hellopeople")
+r = ("hellomom")
+m = ("hellodad")
+
+
+print( "x" == "m" )
+print( "y" == "r" )
+print( "t" == "q" )
+print( "u" == "l" )
+print( "i" == "p" )
+print( "p" == "i" )
+print( "l" == "u" )
+print( "q" == "t" )
+print( "r" == "y" )
+print( "m" == "x" )
+
+
+
+
+
+
