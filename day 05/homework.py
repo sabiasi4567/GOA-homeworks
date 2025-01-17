618ba9c76b01cd2f2c77eac625ce01d359f43b5a
sabias4567 <i.tsitskishvili1987@gmail.com>
2025-01-17 01:51:07 -0800

    commit

---
+++ day 05/homework.py
@@ -0,0 +1,17 @@
+# 1
+current_year = 2024
+print(current_year  + current_year)
+
+# 2
+country ="USA"
+print("you are from " + country)
+
+# 3
+height = 180
+print("Your height in cm is " + str(int (height) / 100) + " meters")
+
+
+# 4
+
+new_name = "Alex"
+print("Hello, " + new_name)
